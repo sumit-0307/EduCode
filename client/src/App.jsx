10b32b4c04a17b25e4b3a01bff0c627fccf3029e
Izumi-03 <sumit9960254325@gmail.com>
2025-03-27 00:02:21 +0530

    created and connected the database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
+import Tasks from './pages/Tasks';
 
 export default function App() {
   return <BrowserRouter>
@@ -14,6 +15,7 @@ export default function App() {
   <Header />
   <Routes>
     <Route path='/' element={<Dashboard/>} />
+    <Route path='tasks' element={<Tasks/>} />
     <Route path='about' element={<About/>} />
     <Route path='edubot' element={<EduBot/>} />
     <Route path='eduler' element={<Eduler/>} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,27 +2,30 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className='bg-slate-400'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+    <div className='bg-slate-300'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-5'>
         <Link to={'/'} className='flex items-center space-x-0'>
-        <h1 className='font-bold'>Edu</h1>
-        <h1 className='font-bold'>Code</h1>
+        <h1 className='text-slate-700 font-bold'>Edu</h1>
+        <h1 className='text-slate-900 font-bold'>Code</h1>
         </Link>
-        <ul className='flex gap-4'>
+        <ul className='flex gap-6'>
           <Link to={'/'}>
           <li>Dashboard</li>
           </Link>
-          <Link to={'/edubot'}>
-          <li>EduBot</li>
+          <Link to={'/tasks'}>
+          <li>Tasks</li>
           </Link>
           <Link to={'/eduler'}>
           <li>Eduler</li>
           </Link>
-          <Link to={'/about'}>
-          <li>About</li>
+          <Link to={'/edubot'}>
+          <li>EduBot</li>
           </Link>
           <Link to={'/profile'}>
           <li>Profile</li>
+          </Link>
+          <Link to={'/about'}>
+          <li>About</li>
           </Link>
           <Link to={'/sign-in'}>
           <li>Sign in</li>
