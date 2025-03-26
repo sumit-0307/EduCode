7c692d5fd488b6037b96dc714f1e7238554772cd
Izumi-03 <sumit9960254325@gmail.com>
2025-03-26 21:20:58 +0530

    added routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import EduBot from './pages/EduBot';
+import Eduler from './pages/Eduler';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Dashboard/>} />
+    <Route path='about' element={<About/>} />
+    <Route path='edubot' element={<EduBot/>} />
+    <Route path='eduler' element={<Eduler/>} />
+    <Route path='profile' element={<Profile/>} />
+    <Route path='sign-in' element={<SignIn/>} />
+    <Route path='sign-up' element={<SignUp/>} />
+  </Routes>
+    </BrowserRouter>
+
 }
---
+++ client/src/pages/EduBot.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EduBot() {
+  return (
+    <div>EduBot</div>
+  )
+}
